f32060ae38940669f60493fe7662d9146f46df38
ScottHAZI <Jinghang.Scott.Hu@gmail.com>
2020-10-20 15:25:25 +0800

    fix mobile scrolling issue

--- assets/js/request-video-landing.js
+++ assets/js/request-video-landing.js
@@ -12,3 +12,26 @@ document.querySelector('.is-playlist-item').click();
 document.querySelector('.is-mobile-playlist-header').addEventListener('click', () => {
   document.getElementById('mobile-info-panel').classList.toggle('is-active');
 });
+
+document.body.addEventListener('click', (e) => {
+  const playlistItem = e.target.closest('.is-playlist-item');
+  if (playlistItem) {
+    const resourcesTab = document.querySelector('.is-resources-tab');
+    const overviewTab = document.querySelector('.is-overview-tab');
+    const resourcesInfo = document.querySelector('.is-resources-info');
+    if (!document.querySelector(`.is-playlist-item-${playlistItem.dataset.idx}-tab-content.is-resources`)) {
+      resourcesTab.classList.remove('is-active');
+      resourcesTab.classList.add('is-hidden');
+      overviewTab.click();
+      resourcesInfo.classList.add('is-hidden');
+    } else {
+      resourcesTab.classList.remove('is-hidden');
+      resourcesInfo.classList.remove('is-hidden');
+    }
+  }
+});
+
+const touchDevice = ('ontouchstart' in document.documentElement);
+if (touchDevice) {
+  document.body.classList.add('is-touch-device');
+}
--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github/vibeus/vibe-website
 go 1.13
 
 require (
-	github.com/vibeus/hugo-common/v2 v2.0.2-0.20201020020731-814b52852179 // indirect
+	github.com/vibeus/hugo-common/v2 v2.0.2-0.20201020032622-e8516850aae7 // indirect
 	github.com/vibeus/vibe-cms/v2 v2.0.1-0.20200908171402-2b67c84e6c44 // indirect
 )
-
