67fdd2a3725d05a96946a170126de386a67d30a6
suukii <suki@vibe.us>
2021-05-13 14:51:47 +0800

    update page content

--- assets/js/toc-template.js
+++ assets/js/toc-template.js
@@ -1,26 +1,24 @@
 /*
 {{ $src := resources.Get "js/common/common.js" | resources.Minify | resources.Fingerprint }}
 */
-import { toggleActive } from '{{ $src.RelPermalink }}';
-
-toggleActive('.dropdown.toc-dropdown', true);
+import { bindEventWithTarget } from '{{ $src.RelPermalink }}';
 
 const updateToc = (function () {
   const tocItems = document.querySelectorAll('.toc [data-id]')
   const subItemsEls = document.querySelectorAll('.toc .subitems')
 
-  return (ativeId) => {
-    tocItems.forEach(el => {
-      const isActive = ativeId === el.dataset.id;
+  return (activeId) => {
+    tocItems.forEach(tocItem => {
+      const isActive = activeId === tocItem.dataset.id;
       const op = isActive ? 'add' : 'remove';
-      el.classList[op]('is-active')
-
+      tocItem.classList[op]('is-active');      
     });
     subItemsEls.forEach(el => {
       const hasActiveChild = el.querySelector('.subitem.is-active');
 
       const op = hasActiveChild ? 'add' : 'remove';
       el.classList[op]('is-expand');
+      el.parentNode.classList[op]('is-expand');
     })
   }
 })()
@@ -41,3 +39,35 @@ function observeHeaders(notify) {
   }, options)
   headers.forEach(item => observer.observe(item));
 }
+
+const navbar = document.querySelector('.navbar');
+const tocDropdown = document.querySelector('.toc-dropdown');
+const tocDropdownMenu = document.getElementById('dropdown-menu');
+
+bindEventWithTarget('.dropdown-trigger', 'click', () => {
+  tocDropdown.classList.toggle('is-active');
+  const isActive = tocDropdown.classList.contains('is-active');
+  if (isActive) {
+    document.documentElement.classList.add('is-clipped');
+    setTocHeight();
+  } else {
+    document.documentElement.classList.remove('is-clipped');
+    clearTocHeight();
+  }
+
+});
+
+function setTocHeight() {
+  const navbarTop = navbar.getBoundingClientRect().top;
+  tocDropdownMenu.style.maxHeight = `calc(${window.innerHeight - navbarTop}px - 4.125rem - 48px)`;
+}
+
+function clearTocHeight() {
+  tocDropdownMenu.style.maxHeight = '';
+}
+
+window.addEventListener('resize', () => {
+  if (tocDropdown.classList.contains('is-active')) {
+    setTocHeight();
+  }
+});
