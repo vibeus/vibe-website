fa9f47f528a4c2ba147a2ff565c34fe01a404483
dzr975336710 <975336710@qq.com>
2021-07-09 17:07:04 +0800

    redesign homepage

--- assets/js/hero-video-fade.js
+++ assets/js/hero-video-fade.js
@@ -1,38 +1,23 @@
-// 观察者的选项(要观察哪些突变)
-const config = { attributes: true, className: true };
-
+/**
+ * hero video 点击播放/关闭 动画效果 给对应Dom元素加上 class "is-active"
+ */
 const heroVideoPlay = document.getElementById("hero-video-play")
 const heroVideoClose = document.getElementById("hero-video-close")
 const homeDom = document.getElementById("home")
 const fadeContainer = document.getElementById("fade-container")
 const navDom = document.getElementsByClassName("navbar")[0]
+const globalNavDom = document.getElementsByClassName("global-before-nav")[0]
 
 heroVideoPlay.onclick = function () {
   homeDom.classList.add("is-active")
   fadeContainer.classList.remove("is-active")
-  navDom.classList.add("is-active")
+  navDom.style.display = "none"
+  globalNavDom.style.display = "none"
 }
 
 heroVideoClose.onclick = function () {
   homeDom.classList.remove("is-active")
   fadeContainer.classList.add("is-active")
-  navDom.classList.remove("is-active")
+  navDom.style.display = "unset"
+  globalNavDom.style.display = "unset"
 }
-
-// const callback = function(mutationsList) {
-//   mutationsList.forEach(function(item,index){
-//     console.log('item: ', item.target);
-//     if (item.target.classList[1] === "is-active") {
-
-//     } else {
-
-//     }
-//   });
-// };
-
-
-// // 创建一个链接到回调函数的观察者实例
-// const heroVideoObserver = new MutationObserver(callback);
-
-// // 开始观察已配置突变的目标节点
-// heroVideoObserver.observe(heroVideoPlay, config)
